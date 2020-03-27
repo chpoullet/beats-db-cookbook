8ac4a3700de7e42c6ad808580a63056f52349d30
chpoullet <59974805+chpoullet@users.noreply.github.com>
2020-03-27 06:32:17 +0000

    Update default_spec.rb

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,18 +6,6 @@
 
 require 'spec_helper'
 
-describe 'beatsdb_cookbook::default' do
-  context 'When all attributes are default, on Ubuntu 18.04' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '18.04'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-require 'spec_helper'
-
 describe 'beatsapp_cookbook::default' do
   context 'When all attributes are default, on Ubuntu 18.04' do
     # for a complete list of available platforms and versions see:
@@ -27,15 +15,17 @@ describe 'beatsapp_cookbook::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
     it 'should update source list' do
       expect(chef_run).to update_apt_update('update_sources')
     end
+    
     it 'metricbeat' do
      expect(chef_run).to install_package "metricbeat"
     end
+    
     it 'filebeat' do
      expect(chef_run).to install_package "filebeat"
     end
+  end
 end
